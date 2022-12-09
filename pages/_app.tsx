1dd5be625e1b6637304f2cb6e04d4193a6d38fd3
Chase Struse <chasestruse@gmail.ocm>
2022-12-09 02:27:27 -0600

    Rearranged home layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import localFont from '@next/font/local'
+
+const customFont = localFont({ src: "../public/fonts/MonaSans/Mona-Sans.woff2", display: "swap",})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={customFont.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,56 @@
-/* eslint-disable react/no-unescaped-entities */
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Image from "next/image";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Strusefolio</title>
-        <meta name="description" content="Portfolio for Chase Struse, Strusefolio" />
+        <meta
+          name="description"
+          content="Portfolio for Chase Struse, Strusefolio"
+        />
         <link rel="icon" href="/images/favicon.jpeg" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Hey, I'm Chase Struse
-        </h1>
-        <p className={styles.description}>
-          Welcome to my Strusefolio!
-        </p>
+        <div className={styles.profile}>
+          <Image
+            priority
+            src="/images/profile.jpeg"
+            height={120}
+            width={120}
+            alt=""
+            className={styles.profileImage}
+          />
+          <h1 className={styles.name}>Chase Struse</h1>
+          <p className={styles.occupation}>Full Stack Developer</p>
+          <div className={styles.socials}>
+            <a href="https://www.github.com/chasestruse">
+              <FontAwesomeIcon icon={faGithub} className={styles.socials}/>
+            </a>
+            <a href="https://www.linkedin.com">
+              <FontAwesomeIcon icon={faLinkedin} className={styles.socials}/>
+            </a>
+
+          </div>
+          <div className={styles.experience}>
+            <h2>Experience</h2>
+            <p>C# / .NET: 4 years</p>
+            <p>React: 1 year</p>
+            <p>Angular: 2 years</p>
+            <p>SQL: 2 years</p>
+            <p>AWS: 1 year</p>
+            <p>GCloud: 1.5 years</p>
+            <p>Jira: 4 years</p>
+            <p>Azure Devops: 1 year</p>
+            <p>Git and Github: 4 years</p>
+          </div>
+        </div>
       </main>
     </div>
-  )
+  );
 }
