7c4d2b8ac8719da494c249f4d93b3d17b99678f8
Chase Struse <chasestruse@gmail.ocm>
2022-12-11 16:10:11 -0600

    Updated cards to look better for projects

--- components/project-card.tsx
+++ components/project-card.tsx
@@ -12,10 +12,9 @@ type ProjectCardProps = {
 export default function ProjectCard({title, description, repoLink, languageUsed}: ProjectCardProps){
     return(
         <div className={styles.projectCard}>
-            <h2>{title}</h2>
+            <a href={repoLink}>{title} <FontAwesomeIcon icon={faGithub} className={styles.icon}/></a>
             <h6>Languages/Frameworks Used: {languageUsed}</h6>
             <p>{description}</p>
-            <a href={repoLink}><FontAwesomeIcon icon={faGithub} className={styles.icon}/></a>
         </div>
     )
 }
--- components/projects.tsx
+++ components/projects.tsx
@@ -6,48 +6,31 @@ export default function Projects() {
     {
       id: 1,
       title: "National Parks Command Line App",
-      description: "Command line app that gives information on national parks",
+      description: "Command line app that gives information on national parks. Useful for anyone who is looking to travel and spend time at a National Park.",
       repoLink: "https://www.github.com/ChaseStruse/national-parks-cla",
       languageUsed: "Python",
     },
     {
       id: 2,
       title: "Basketball Bot",
-      description: "Discord bot that can show NBA stats based on the command",
+      description: "Discord bot that can show NBA stats based on the command. This is great to use to see the scores of todays games or even see the current standings within the NBA. (Go Celtics!)",
       repoLink: "https://www.github.com/ChaseStruse/basketball-bot",
       languageUsed: "Python",
     },
     {
       id: 3,
       title: "Weather Bot",
-      description: "Discord bot that shows the forecasted temperatures",
+      description: "Discord bot that shows the forecasted temperatures. While it only allows you to see temperatures currently, in future releases I am looking to add percipitation.",
       repoLink: "https://www.github.com/ChaseStruse/weather-bot",
       languageUsed: "Python",
     },
     {
       id: 4,
       title: "NextJs Blog",
-      description:
-        "Blog site created following the Next.js tutorial and expanded from there",
+      description: "Blog site created following the Next.js tutorial and expanded from there. This is my introduction into nextjs and after this tutorial I fell in love with it!",
       repoLink: "https://www.github.com/ChaseStruse/nextjs-blog",
       languageUsed: "Next.js",
     },
-    {
-        id: 5,
-        title: "NextJs Blog",
-        description:
-          "Blog site created following the Next.js tutorial and expanded from there",
-        repoLink: "https://www.github.com/ChaseStruse/nextjs-blog",
-        languageUsed: "Next.js",
-      },
-      {
-        id: 6,
-        title: "NextJs Blog",
-        description:
-          "Blog site created following the Next.js tutorial and expanded from there",
-        repoLink: "https://www.github.com/ChaseStruse/nextjs-blog",
-        languageUsed: "Next.js",
-      },
   ];
   return (
     <div className={style.projectContainer}>
