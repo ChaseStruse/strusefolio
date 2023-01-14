3e3fe8469501673a33a0a4424f180a55c6362d13
Chase Struse <chasestruse@gmail.ocm>
2023-01-14 17:21:31 -0600

    Added reason for contact field

--- components/contact/contact-card.tsx
+++ components/contact/contact-card.tsx
@@ -6,21 +6,26 @@ export default function ContactCard() {
       <h1>Contact</h1>
       <form>
         <div className={styles.cardInput}>
-        <label>
-          Name:
-          <input type="text" name="name" required />
-        </label>
-        <label>
-          Email:
-          <input
-            type="email"
-            name="email"
-            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
-          />
-        </label>
+          <label>
+            Name:
+            <input type="text" name="name" required />
+          </label>
+          <label>
+            Email:
+            <input
+              type="email"
+              name="email"
+              pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+              required
+            />
+          </label>
+          <label>
+            Reason For Contact:
+            <input type="text" name="reasonForContact" required style={{height: "100px"}}/>
+          </label>
         </div>
         <div className={styles.cardButton}>
-        <input type="submit" value="Submit" />
+          <input type="submit" value="Submit" />
         </div>
       </form>
     </div>
