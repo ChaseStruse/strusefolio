a1f936be6152d38012e3c6f190296190e5434cae
Chase Struse <chasestruse@gmail.ocm>
2023-01-13 23:27:52 -0600

    Added contact input form

--- components/contact/contact-card.tsx
+++ components/contact/contact-card.tsx
@@ -1,10 +1,28 @@
 import styles from "./contact.module.css";
 
-export default function ContactCard(){
+export default function ContactCard() {
   return (
     <div className={styles.card}>
       <h1>Contact</h1>
-      <p>Not currently working</p>
+      <form>
+        <div className={styles.cardInput}>
+        <label>
+          Name:
+          <input type="text" name="name" required />
+        </label>
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+          />
+        </label>
+        </div>
+        <div className={styles.cardButton}>
+        <input type="submit" value="Submit" />
+        </div>
+      </form>
     </div>
-  )
+  );
 }
